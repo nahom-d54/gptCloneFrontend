a026a8ed4e213bb4bafd1dc7cdbd57c44c9eefd5
Nahom d <nahom.d54@gmail.com>
2024-11-29 01:06:59 +0300

    implement authentication redirection in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,33 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "./App.css";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
 
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.token && window.location.pathname === "/login") {
+      navigate("/");
+    }
+    if (
+      !auth.token &&
+      window.location.pathname !== "/auth/login" &&
+      window.location.pathname !== "/auth/register"
+    ) {
+      navigate("/auth/login");
+    }
+  }, [auth, navigate]);
+
   return (
     <div className="bg-[#f4f4f4] dark:bg-gray-900">
-      <Outlet />
+      {auth.token && <Outlet />}
     </div>
   );
 };
