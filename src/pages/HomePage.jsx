a85db991df8e13677a94da3f6e8478e344a5c92f
Nahom d <nahom.d54@gmail.com>
2024-11-29 01:09:21 +0300

    refactor HomePage component to include Chat and Sidebar; implement toggle functionality for MobileSidebar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,28 @@
-import Navbar from "../components/Navigation/Navbar";
-import MainContent from "../components/Home/MainContent";
-import HeroSection from "../components/ExchangeRates/HeroSection";
+import { useState } from "react";
+import Chat from "../components/Chat";
+import MobileSiderbar from "../components/MobileSidebar";
+import Sidebar from "../components/Sidebar";
 
-const HomePage = () => {
+//import useAnalytics from "../hooks/useAnalytics";
+
+export default function Home() {
+  const [isComponentVisible, setIsComponentVisible] = useState(false);
+
+  const toggleComponentVisibility = () => {
+    setIsComponentVisible(!isComponentVisible);
+  };
+
   return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <MainContent />
-    </div>
+    <main className="overflow-hidden w-full h-screen relative flex">
+      {isComponentVisible ? (
+        <MobileSiderbar toggleComponentVisibility={toggleComponentVisibility} />
+      ) : null}
+      <div className="dark hidden flex-shrink-0 bg-gray-900 md:flex md:w-[260px] md:flex-col">
+        <div className="flex h-full min-h-0 flex-col ">
+          <Sidebar />
+        </div>
+      </div>
+      <Chat toggleComponentVisibility={toggleComponentVisibility} />
+    </main>
   );
-};
-
-export default HomePage;
+}
