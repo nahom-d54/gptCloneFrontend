8bab78529a1ecc0144c13eb019512d9c39a35540
Nahom d <nahom.d54@gmail.com>
2024-11-29 01:07:28 +0300

    add chat reducer to rootReducer for chat feature integration

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { api } from "../services/api";
 import authReducer from "../features/auth/authSlice";
 import modalReducer from "../features/modal/modalslice";
+import chatReducer from "../features/chats/chatSlice";
 
 const rootReducer = combineReducers({
   [api.reducerPath]: api.reducer,
   auth: authReducer,
   modal: modalReducer,
+  chat: chatReducer,
 });
 export default rootReducer;
