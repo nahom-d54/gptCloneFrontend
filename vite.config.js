5296788d46666fdc3ab11522d592108169940bab
Nahom d <nahom.d54@gmail.com>
2024-11-29 01:06:47 +0300

    update Vite configuration to change API proxy target to localhost

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        target: "https://api.exchango.pro.et",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
