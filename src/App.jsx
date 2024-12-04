2519230bd4b42cdeb639cd587276a808d5904f1d
Nahom d <nahom.d54@gmail.com>
2024-12-04 20:31:43 +0300

    feat(App): implement dark mode toggle and update background styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 // src/App.js
 import { useEffect } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./App.css";
 
 import { Outlet, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useDarkMode } from "./hooks/useDarkMode";
 
 const App = () => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
+
+  const [isDarkMode, toggleDarkMode] = useDarkMode();
+  useEffect(() => {
+    document.body.className = isDarkMode ? "dark" : "";
+  }, [isDarkMode]);
 
   const auth = useSelector((state) => state.auth);
   const navigate = useNavigate();
@@ -29,7 +36,17 @@ const App = () => {
   }, [auth, navigate]);
 
   return (
-    <div className="bg-[#f4f4f4] dark:bg-gray-900">
+    <div className="bg-white dark:bg-gray-900">
+      <button
+        onClick={toggleDarkMode}
+        className="z-[1000] fixed top-4 right-4 p-2 bg-gray-200 dark:bg-gray-700 rounded-full focus:outline-none"
+      >
+        {isDarkMode ? (
+          <FaSun className="text-yellow-400" />
+        ) : (
+          <FaMoon className="text-gray-500" />
+        )}
+      </button>
       {auth.token && <Outlet />}
     </div>
   );
