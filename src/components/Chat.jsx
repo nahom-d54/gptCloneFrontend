7ffecda91701a65dde83f159de936dd866e2dbd7
Nahom d <nahom.d54@gmail.com>
2024-12-04 20:31:56 +0300

    feat(Chat): enhance dark mode support and improve component styling

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -105,7 +105,7 @@ const Chat = (props) => {
   }, [showEmptyChat, conversation]);
 
   return (
-    <div className="dark flex max-w-full flex-1 flex-col">
+    <div className="flex max-w-full flex-1 flex-col bg-white dark:bg-gray-800">
       <button
         onClick={toggleDarkMode}
         className="absolute top-4 right-4 p-2 bg-gray-200 dark:bg-gray-700 rounded-full focus:outline-none"
@@ -117,24 +117,24 @@ const Chat = (props) => {
         )}
       </button>
 
-      <div className="sticky top-0 z-10 flex items-center border-b border-white/20 bg-gray-800 pl-1 pt-1 text-gray-200 sm:pl-3 md:hidden">
+      <div className="sticky top-0 z-10 flex items-center border-b border-gray-300 dark:border-white/20 bg-gray-100 dark:bg-gray-800 pl-1 pt-1 text-gray-800 dark:text-gray-200 sm:pl-3 md:hidden">
         <button
           type="button"
-          className="-ml-0.5 -mt-0.5 inline-flex h-10 w-10 items-center justify-center rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white dark:hover:text-white"
+          className="-ml-0.5 -mt-0.5 inline-flex h-10 w-10 items-center justify-center rounded-md hover:text-gray-900 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           onClick={toggleComponentVisibility}
         >
           <span className="sr-only">Open sidebar</span>
-          <RxHamburgerMenu className="h-6 w-6 text-white" />
+          <RxHamburgerMenu className="h-6 w-6 text-gray-800 dark:text-white" />
         </button>
-        <h1 className="flex-1 text-center text-base font-normal">New chat</h1>
+        <h1 className="flex-1 text-center text-base font-normal"></h1>
         <button type="button" className="px-3">
-          <BsPlusLg className="h-6 w-6" />
+          <BsPlusLg className="h-6 w-6 text-gray-800 dark:text-white" />
         </button>
       </div>
       <div className="relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1">
         <div className="flex-1 overflow-hidden">
-          <div className="h-full dark:bg-gray-800 overflow-y-auto">
-            <div className="flex flex-col items-center text-sm bg-gray-800">
+          <div className="h-full bg-gray-100 dark:bg-gray-800 overflow-y-auto">
+            <div className="flex flex-col items-center text-sm bg-gray-100 dark:bg-gray-800">
               {!showEmptyChat && conversation?.length > 0 ? (
                 <>
                   <InfiniteScroll
@@ -155,7 +155,7 @@ const Chat = (props) => {
               ) : null}
               {showEmptyChat ? (
                 <div className="py-10 relative w-full flex flex-col h-full">
-                  <h1 className="text-2xl sm:text-4xl font-semibold text-center text-gray-200 dark:text-gray-600 flex gap-2 items-center justify-center h-screen">
+                  <h1 className="text-2xl sm:text-4xl font-semibold text-center text-gray-800 dark:text-gray-600 flex gap-2 items-center justify-center h-screen">
                     GROK
                   </h1>
                 </div>
@@ -163,7 +163,7 @@ const Chat = (props) => {
             </div>
           </div>
         </div>
-        <div className="absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2">
+        <div className="absolute bottom-0 left-0 w-full border-t md:border-t-0 border-gray-300 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2">
           <form className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
             <div className="relative flex flex-col h-full flex-1 items-stretch md:flex-col">
               {errorMessage ? (
@@ -173,7 +173,7 @@ const Chat = (props) => {
                   </div>
                 </div>
               ) : null}
-              <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
+              <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-gray-300 dark:border-gray-900/50 bg-white dark:bg-gray-700 text-gray-800 dark:text-white rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
                 <textarea
                   ref={textAreaRef}
                   value={message}
@@ -194,12 +194,12 @@ const Chat = (props) => {
                   onClick={sendMessage}
                   className="absolute p-1 rounded-md bottom-1.5 md:bottom-2.5 bg-transparent disabled:bg-gray-500 right-1 md:right-2 disabled:opacity-40"
                 >
-                  <FiSend className="h-4 w-4 mr-1 text-white " />
+                  <FiSend className="h-4 w-4 mr-1 text-gray-800 dark:text-white" />
                 </button>
               </div>
             </div>
           </form>
-          <div className="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
+          <div className="px-3 pt-2 pb-3 text-center text-xs text-gray-500 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
             <span>
               ChatGPT Clone may produce inaccurate information about people,
               places, or facts.
