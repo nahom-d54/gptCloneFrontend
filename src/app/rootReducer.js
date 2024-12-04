34cc97673267a97c187f31595116226179ed6e13
Nahom d <nahom.d54@gmail.com>
2024-12-04 19:07:05 +0300

    add(rootReducer): include chatHistoryReducer for managing chat history

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -3,11 +3,13 @@ import { api } from "../services/api";
 import authReducer from "../features/auth/authSlice";
 import modalReducer from "../features/modal/modalslice";
 import chatReducer from "../features/chats/chatSlice";
+import chatHistoryReducer from "../features/chats/chatHistorySlice";
 
 const rootReducer = combineReducers({
   [api.reducerPath]: api.reducer,
   auth: authReducer,
   modal: modalReducer,
   chat: chatReducer,
+  chatHistory: chatHistoryReducer,
 });
 export default rootReducer;
